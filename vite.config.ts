b27e31908254a4ba1c05b29c9aa69fde3e7914cf
Danny <danny.belmonte4@gmail.com>
2025-07-30 21:48:09 -0500

    updated base path for now

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/project-e-client/' : '/'
+  const base = mode === 'production' ? '/project-e-client/' : '/project-e-client/'
   
   return {
     plugins: [react()],
