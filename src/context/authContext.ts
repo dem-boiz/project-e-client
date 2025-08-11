da6a47fa4b418745d4a64834cfe583c2f99c606d
Danny <danny.belmonte4@gmail.com>
2025-08-10 20:28:33 -0500

    updated authcontextype and user

--- src/context/authContext.ts
+++ src/context/authContext.ts
@@ -6,11 +6,13 @@ interface User {
   email: string;
   token_type: string;
   access_token: string;
+  refresh_token: string;
 }
 
 export interface AuthContextType {
   user: User | null;
-  token: string | null;
+  accessToken: string | null;
+  refreshToken: string | null;
   login: (userData: User, rememberMe?: boolean) => void;
   logout: () => void;
   isAuthenticated: () => boolean;
