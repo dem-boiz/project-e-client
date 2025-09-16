66b8fcbcc0965dbb97fd5e18a0a1a6f7c85a3e07
Danny <danny.belmonte4@gmail.com>
2025-09-15 20:44:10 -0500

    userDrawer hook

---
+++ src/context/useDrawer.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { DrawerContext } from './DrawerContext';
+
+export const useDrawer = () => {
+  const context = useContext(DrawerContext);
+  if (context === undefined) {
+    throw new Error('useDrawer must be used within a DrawerProvider');
+  }
+  return context;
+};
